01d525710a978e7c4199d0a51a8875b6f94198ce
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-30 07:46:00 +0100

    l'implémentation del'entite fournisseur

--- tricol/src/main/java/com/tapprovisionnement/tricol/model/Fournisseur.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/model/Fournisseur.java
@@ -1,4 +1,34 @@
 package com.tapprovisionnement.tricol.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "fournisseurs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Fournisseur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String societe;
+
+    private String adresse;
+
+    private String contact;
+
+    private String email;
+
+    private String telephone;
+
+    private String ville;
+
+    @Column(name = "ice", unique = true)
+    private String ICE;
 }
