5d1f6ccb7ce43e8ef02330b1a14f108833209d70
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-30 10:13:48 +0100

    l'implémentation de l'interface ProduitMapper

--- tricol/src/main/java/com/tapprovisionnement/tricol/mapper/ProduitMapper.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/mapper/ProduitMapper.java
@@ -1,4 +1,16 @@
 package com.tapprovisionnement.tricol.mapper;
 
+import com.tapprovisionnement.tricol.model.Produit;
+import com.tapprovisionnement.tricol.dto.ProduitDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface ProduitMapper {
+
+    ProduitMapper INSTANCE = Mappers.getMapper(ProduitMapper.class);
+
+    ProduitDTO toDTO(Produit produit);
+
+    Produit toEntity(ProduitDTO produitDTO);
 }
