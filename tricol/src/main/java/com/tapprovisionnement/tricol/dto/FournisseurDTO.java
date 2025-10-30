87f7401a2907f5d296aff635053c26fabac1cd8d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-10-30 10:11:01 +0100

    la'implémentaion de la classe FournisseurDTO

--- tricol/src/main/java/com/tapprovisionnement/tricol/dto/FournisseurDTO.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/dto/FournisseurDTO.java
@@ -1,4 +1,20 @@
 package com.tapprovisionnement.tricol.dto;
 
-public class FournisseurDto {
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FournisseurDTO {
+
+    private int id;
+    private String societe;
+    private String adresse;
+    private String contact;
+    private String email;
+    private String telephone;
+    private String ville;
+    private String ICE;
 }
