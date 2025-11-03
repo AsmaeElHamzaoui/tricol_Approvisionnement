5cbfb2fb2a517e10a6f74eb359e5e463a9c2bba7
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-03 15:13:27 +0100

    l'implémentation des fonctionnalités pour la récupération de tout les commande ou bien une commande précise

--- tricol/src/main/java/com/tapprovisionnement/tricol/service/CommandeService.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/service/CommandeService.java
@@ -1,4 +1,32 @@
 package com.tapprovisionnement.tricol.service;
 
+import com.tapprovisionnement.tricol.dto.CommandeDTO;
+import com.tapprovisionnement.tricol.mapper.CommandeMapper;
+import com.tapprovisionnement.tricol.model.Commande;
+import com.tapprovisionnement.tricol.repository.CommandeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommandeService {
+    private final CommandeRepository commandeRepository;
+    private final CommandeMapper commandeMapper;
+
+    //getAll commandes with pagination sorting by id ascending
+    public Page<CommandeDTO> getAllCommandes(int page,int nbrElement){
+        Pageable pageable= PageRequest.of(page,nbrElement, Sort.by("id").ascending());
+        Page<Commande> commandes=commandeRepository.findAll(pageable);
+        return commandes.map(commandeMapper::toDTO);
+    }
+
+    //getByid
+    public CommandeDTO getById(int id){
+       Commande commande =commandeRepository.findById(id).orElseThrow(()->new RuntimeException("Commande not found"));
+       return commandeMapper.toDTO(commande);
+    }
 }
