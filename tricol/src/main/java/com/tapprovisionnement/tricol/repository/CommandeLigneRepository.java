0954bc2bb98408176d97ea2ace8361cab8df4903
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-03 14:18:57 +0100

    la  création du repository du commandeLigne

---
+++ tricol/src/main/java/com/tapprovisionnement/tricol/repository/CommandeLigneRepository.java
@@ -0,0 +1,7 @@
+package com.tapprovisionnement.tricol.repository;
+
+import com.tapprovisionnement.tricol.model.CommandeLigne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommandeLigneRepository extends JpaRepository<CommandeLigne,Integer> {
+}
