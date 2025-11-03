787c57bcb04308ccee16bb9a42ef5feaf221303a
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-03 15:01:07 +0100

    l'implémentation du mapper CommandeLigneMapper

--- tricol/src/main/java/com/tapprovisionnement/tricol/mapper/CommandeLigneMapper.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/mapper/CommandeLigneMapper.java
@@ -1,4 +1,12 @@
 package com.tapprovisionnement.tricol.mapper;
 
+
+import com.tapprovisionnement.tricol.dto.CommandeLigneDTO;
+import com.tapprovisionnement.tricol.model.CommandeLigne;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CommandeLigneMapper {
+    CommandeLigneDTO toDTO(CommandeLigne commandeLigne);
+    CommandeLigne toEntiry(CommandeLigneDTO commandeLigneDTO);
 }
