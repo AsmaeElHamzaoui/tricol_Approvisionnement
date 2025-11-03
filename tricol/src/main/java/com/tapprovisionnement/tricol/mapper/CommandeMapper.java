a5d9d2cc4121da2b86cfee0a6fa025ac39cea21c
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-03 14:59:04 +0100

    l'implémentation du mapper CommandeMapper

--- tricol/src/main/java/com/tapprovisionnement/tricol/mapper/CommandeMapper.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/mapper/CommandeMapper.java
@@ -1,4 +1,11 @@
 package com.tapprovisionnement.tricol.mapper;
 
+import com.tapprovisionnement.tricol.dto.CommandeDTO;
+import com.tapprovisionnement.tricol.model.Commande;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CommandeMapper {
+    CommandeDTO  toDTO(Commande commande);
+    Commande toEntity(CommandeDTO commandeDTO);
 }
