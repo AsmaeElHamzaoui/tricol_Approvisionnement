88b5386cf1ead3e814c689c9129d4adc39ebbd3c
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-03 16:00:54 +0100

    l'implémentation des fonctionnalités pour lr l'ajout et la modification des commandes

--- tricol/src/main/java/com/tapprovisionnement/tricol/service/CommandeService.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/service/CommandeService.java
@@ -3,7 +3,9 @@ package com.tapprovisionnement.tricol.service;
 import com.tapprovisionnement.tricol.dto.CommandeDTO;
 import com.tapprovisionnement.tricol.mapper.CommandeMapper;
 import com.tapprovisionnement.tricol.model.Commande;
+import com.tapprovisionnement.tricol.model.Fournisseur;
 import com.tapprovisionnement.tricol.repository.CommandeRepository;
+import com.tapprovisionnement.tricol.repository.FournisseurRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class CommandeService {
     private final CommandeRepository commandeRepository;
     private final CommandeMapper commandeMapper;
+    private final FournisseurRepository fournisseurRepository;
 
     //getAll commandes with pagination sorting by id ascending
     public Page<CommandeDTO> getAllCommandes(int page,int nbrElement){
@@ -29,4 +32,25 @@ public class CommandeService {
        Commande commande =commandeRepository.findById(id).orElseThrow(()->new RuntimeException("Commande not found"));
        return commandeMapper.toDTO(commande);
     }
+
+    //save
+    public CommandeDTO createCommande(CommandeDTO commandeDTO){
+       Commande commande=commandeMapper.toEntity(commandeDTO);
+       Commande saved=commandeRepository.save(commande);
+       return commandeMapper.toDTO(saved);
+    }
+
+    //update
+    public CommandeDTO updateCommande(int id,CommandeDTO commandeDTO){
+        Commande commande=commandeRepository.findById(id).orElseThrow(()->new RuntimeException("Commande not found"));
+        Fournisseur fournisseur=fournisseurRepository.findById(commandeDTO.getFournisseurId()).orElseThrow(()->new RuntimeException("fournisseur not found"));
+        commande.setDate(commandeDTO.getDate());
+        commande.setStatut(commandeDTO.getStatut());
+        commande.setMontantTotal(commandeDTO.getMontantTotal());
+        commande.setFournisseur(fournisseur);
+        Commande saved=commandeRepository.save(commande);
+        return commandeMapper.toDTO(saved);
+    }
+
+
 }
