0f9565e3c1b5e3b234fc34de05369c5491ad5646
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-05 09:44:13 +0100

    la'implémentation de mapper Mouvement Stock Mapper

--- tricol/src/main/java/com/tapprovisionnement/tricol/mapper/MouvementStockMapper.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/mapper/MouvementStockMapper.java
@@ -1,4 +1,16 @@
 package com.tapprovisionnement.tricol.mapper;
 
-public class MouvementStockMapper {
+import com.tapprovisionnement.tricol.dto.MouvementStockDTO;
+import com.tapprovisionnement.tricol.model.MouvementStock;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MouvementStockMapper {
+
+    @Mapping(source = "ligneCommande.id", target = "ligneCommandeId")
+    MouvementStockDTO toDTO(MouvementStock mouvementStock);
+
+    @Mapping(source = "ligneCommandeId", target = "ligneCommande.id")
+    MouvementStock toEntity(MouvementStockDTO mouvementStockDTO);
 }
