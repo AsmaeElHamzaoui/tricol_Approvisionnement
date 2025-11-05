e4cfc2f952d29c60b7fe16d1d78dfe91bac7246c
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-05 09:37:44 +0100

    lal'implémentation  de l'entité mouvement de stock

--- tricol/src/main/java/com/tapprovisionnement/tricol/model/MouvementStock.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/model/MouvementStock.java
@@ -1,4 +1,33 @@
 package com.tapprovisionnement.tricol.model;
 
+import com.tapprovisionnement.tricol.enums.TypeMouvement;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "mouvements_stock")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MouvementStock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate dateMouvement;
+
+    @Enumerated(EnumType.STRING)
+    private TypeMouvement typeMouvement;
+
+    private int quantite;
+
+    // relation avec ligne commande
+    @ManyToOne
+    @JoinColumn(name = "id_ligne_commande", nullable = false)
+    private CommandeLigne ligneCommande;
 }
