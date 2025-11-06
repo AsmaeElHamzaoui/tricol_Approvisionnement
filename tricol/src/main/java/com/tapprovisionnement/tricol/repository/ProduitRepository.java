5f8cd894b77c149359f0ec18c394bbd1c1285fa4
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-06 17:22:28 +0100

    l'ajout de la signature de la methode findByNom dans le repo du produit

--- tricol/src/main/java/com/tapprovisionnement/tricol/repository/ProduitRepository.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/repository/ProduitRepository.java
@@ -4,4 +4,5 @@ import com.tapprovisionnement.tricol.model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitRepository extends JpaRepository<Produit,Integer> {
+    Produit findByNom(String nom);
 }
