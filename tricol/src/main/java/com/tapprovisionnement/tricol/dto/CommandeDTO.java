c2105c60d9f35bdc093bd0ce74c1710f8ff50422
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-06 17:15:04 +0100

    la'ajout de la proprièèté READ_ONLY à l'attribut prixAchat dans le dto de la commande

--- tricol/src/main/java/com/tapprovisionnement/tricol/dto/CommandeDTO.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/dto/CommandeDTO.java
@@ -1,5 +1,6 @@
 package com.tapprovisionnement.tricol.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tapprovisionnement.tricol.enums.StatutCommande;
 import lombok.*;
 
@@ -13,6 +14,8 @@ public class CommandeDTO {
     private int id;
     private LocalDateTime dateCommande;
     private StatutCommande statut;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private double montantTotal;
     private int fournisseurId;
 }
