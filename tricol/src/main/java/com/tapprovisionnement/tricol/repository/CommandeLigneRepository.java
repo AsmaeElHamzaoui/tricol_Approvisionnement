1a27a1b9bed82248f26d836ce8abaa3b8a42cc84
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-06 17:22:56 +0100

    l'ajout de la signature de la methode findByCommande dans le repo du commandeLigne

--- tricol/src/main/java/com/tapprovisionnement/tricol/repository/CommandeLigneRepository.java
+++ tricol/src/main/java/com/tapprovisionnement/tricol/repository/CommandeLigneRepository.java
@@ -1,7 +1,11 @@
 package com.tapprovisionnement.tricol.repository;
 
+import com.tapprovisionnement.tricol.model.Commande;
 import com.tapprovisionnement.tricol.model.CommandeLigne;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommandeLigneRepository extends JpaRepository<CommandeLigne,Integer> {
+    List<CommandeLigne> findByCommande(Commande commande);
 }
